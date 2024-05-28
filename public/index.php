0cfa4dfb654f15c88cfc5a64a37304eaa1749f34
Aestheria <somsanith.marine.pro@gmail.com>
2024-05-28 16:57:54 +0200

    Index valide W3C

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+ <head>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Mon super TP</title>
+ <link href="css/bootstrap.css" rel="stylesheet">
+ <link href="css/style.css" rel="stylesheet">
+ <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+ </head>
+ <body>
+<!-- Mon super index -->
+ <h1>Mon super TP</h1>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+ </body>
+</html>
